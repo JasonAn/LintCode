8890a8cdf7a17ff7ac3b1afa9da0168e9cffab23
Zhe(Jason) An <anzhe0629@gmail.com>
2016-12-19 01:54:59 -0800

    fixed 28

--- 28/main.cpp
+++ 28/main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
 
+class Solution {
+public:
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+
+        int i = 0, j = matrix.size()-1;
+
+        if (matrix.size() == 0 || matrix[0].size() == 0)
+            return false;
+
+        int m = i, n = i, k = matrix[0].size()-1;
+
+        while(j > i + 1 && !(matrix[m][0] <= target && matrix[m][k] >= target))
+        {
+            // cout << i << " " << m << endl;
+            m = (i + j) / 2;
+
+            if (matrix[m][0] > target)
+                j = m;
+            else
+                i = m;
+        }
+
+        if (!(matrix[m][0] <= target && matrix[m][k] >= target))
+            m = j;
+
+        //cout << m << endl;
+
+        while(k > i + 1 && matrix[m][n] != target){
+            //cout << i << " " << k << endl;
+            n = (i + k) / 2;
+
+            if (matrix[m][n] == target)
+                return true;
+            else if(matrix[m][n] < target)
+                i = n;
+            else
+                k = n;
+        }
+
+        if (matrix[m][n] == target or matrix[m][k] == target)
+            return true;
+        else
+            return false;
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
--- 75/main.cpp
+++ 75/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
+class Solution {
+public:
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    int findPeak(vector<int> A) {
+        int l = 0;
+        int r = A.size()-1;
+
+        int mid = 1;
+
+        while(mid > 0 && mid < A.size() - 1){
+
+            // cout << l << " " << r << endl;
+
+            mid = (l + r) / 2;
+
+            if (A[mid] > A[mid-1] && A[mid] > A[mid+1])
+            {
+                // cout << "success" << endl;
+                return mid;
+            }
+            else if (A[mid] < A[mid-1])
+                r = mid;
+            else
+                l = mid;
+
+        }
+
+
+    }
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
